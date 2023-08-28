66d0b6b4beec4ba4384170bf006d2e8d8feeb24e
Adnan Mushtaq <adnan_mushtaq@strategisthub.com>
2023-08-28 18:56:29 +0500

    feat: cache layer added

---
+++ src/lib/services/logger.service.ts
@@ -0,0 +1,38 @@
+import { createLogger, transports, format} from "winston";
+import type { Logger } from "winston";
+const { combine, timestamp, label, printf, colorize} = format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level.toUpperCase()}: ${message}`;
+});
+class LoggerService {
+  //pirvate methods and properites
+  private static instance: Logger | null = null;
+  //public
+  public static getInstance(): Logger {
+    if (!LoggerService.instance) {
+      LoggerService.instance = createLogger({
+        format: combine(
+          label({ label:"SERVER" }),
+          timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+          colorize({all:true}),
+          myFormat,
+        ),
+        transports: [
+          new transports.File({
+            filename: "../logs/error.log",
+            level: "error",
+            maxsize:5242880, //5mb
+          }),
+          new transports.Console(),
+        ],
+        defaultMeta: {
+          service: "smart-fintrack",
+        },
+      });
+    }
+    return LoggerService.instance;
+  }
+}
+
+export const logger = LoggerService.getInstance();
