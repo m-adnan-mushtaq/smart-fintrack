f86d170a98d136b156d879e3a30583378d7ee826
Adnan Mushtaq <adnan_mushtaq@strategisthub.com>
2023-08-30 20:26:20 +0500

    feat: updated logger service

--- src/lib/services/logger.service.ts
+++ src/lib/services/logger.service.ts
@@ -1,35 +1,26 @@
-import { createLogger, transports, format} from "winston";
-import type { Logger } from "winston";
-const { combine, timestamp, label, printf, colorize} = format;
+import * as log4js from "log4js";
+import type { Logger } from "log4js";
 
-const myFormat = printf(({ level, message, label, timestamp }) => {
-  return `${timestamp} [${label}] ${level.toUpperCase()}: ${message}`;
-});
 class LoggerService {
   //pirvate methods and properites
   private static instance: Logger | null = null;
   //public
   public static getInstance(): Logger {
     if (!LoggerService.instance) {
-      LoggerService.instance = createLogger({
-        format: combine(
-          label({ label:"SERVER" }),
-          timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-          colorize({all:true}),
-          myFormat,
-        ),
-        transports: [
-          new transports.Console(),
-          new transports.File({
+      log4js.configure({
+        appenders: {
+          console: { type: "console", layout: { type: "coloured" } },
+          fileLogs: {
+            type: "file",
             filename: "../logs/error.log",
-            level: "error",
-            maxsize:5242880, //5mb
-          }),
-        ],
-        defaultMeta: {
-          service: "smart-fintrack",
+            maxLogSize: "10M",
+          },
+        },
+        categories: {
+          default: { appenders: ["console", "fileLogs"], level: "error" },
         },
       });
+      LoggerService.instance = log4js.getLogger();
     }
     return LoggerService.instance;
   }
